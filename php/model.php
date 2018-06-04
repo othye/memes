05e71580e2dcec8e7322c0906709facbfcbbf02d
PierreHermey <pierre.hermey@laposte.net>
2018-06-04 15:15:56 +0200

    merge

--- php/model.php
+++ php/model.php
@@ -2,18 +2,18 @@
 <?php
 
 function getBases($pdo): array {
-
-  $statement = $pdo->getInstance()->prepare("SELECT * FROM bases");
-  $statement->execute();
-
-  $data = $statement->fetchAll();
-
-  // echo '<pre>';
-  // var_dump($data);
-  // echo '</pre>';
-
-  return $data;
-
+    
+    $statement = $pdo->getInstance()->prepare("SELECT * FROM bases");
+    $statement->execute();
+    
+    $data = $statement->fetchAll();
+    
+    // echo '<pre>';
+    // var_dump($data);
+    // echo '</pre>';
+    
+    return $data;
+    
 }
 
 ?>
