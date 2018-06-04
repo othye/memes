bc5faf78cef3cf071c8a1e1e04281484b76caad5
knorjador <headers@protonmail.com>
2018-06-04 12:59:08 +0100

    Add Carousel

---
+++ assets/js/create.js
@@ -0,0 +1,17 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+   const   elems     = document.querySelectorAll('.carousel')
+         , options   = {
+
+               dist: 0
+             , numVisible: 8
+
+           }
+         , instances = M.Carousel.init(elems, options)
+
+ })
+
+
+ var canvas = document.getElementById('canvas');
+ var ctx = canvas.getContext('2d');
--- php/model.php
+++ php/model.php
@@ -1,6 +1,19 @@
 
 <?php
 
+function getBases($pdo): array {
 
+  $statement = $pdo->getInstance()->prepare("SELECT * FROM bases");
+  $statement->execute();
+
+  $data = $statement->fetchAll();
+
+  // echo '<pre>';
+  // var_dump($data);
+  // echo '</pre>';
+
+  return $data;
+
+}
 
 ?>
