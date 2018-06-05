a6b98d5d17e104e6111112ccb28058b894b3c8ea
PierreHermey <pierre.hermey@laposte.net>
2018-06-05 11:43:38 +0200

    Graphic update

--- assets/js/create.js
+++ assets/js/create.js
@@ -110,3 +110,8 @@ const init = function() {
   checkForm()
 
 }()
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems);
+});
